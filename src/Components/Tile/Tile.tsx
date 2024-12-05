5bbd6794c9fa5676b1e9ad7a42e78ee100dc3fb7
AndryChristian <39271749+chris15900@users.noreply.github.com>
2024-12-05 13:11:11 +0300

    Add Movements

--- src/Components/Tile/Tile.tsx
+++ src/Components/Tile/Tile.tsx
@@ -8,12 +8,12 @@ interface Props {
 export default function Tile({number, image }: Props) {
     if(number % 2 === 0 ) {
         return <div className='tile black-tile'>
-            <img src={image}/>
-            </div>;
+            {image && <div style={{backgroundImage: `url(${image})` }} className="chess-piece"></div>}
+            </div>
     } else {
         return <div className='tile white-tile'>
-            <img src={image}/>
-        </div>;
+             {image && <div style={{backgroundImage: `url(${image})` }} className="chess-piece"></div>}
+        </div>
     }
     
 } 
